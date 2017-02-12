87b1f7653e551d3fe73b216867f93d0ba2c0c169
Sanal <sanalpanicker1987@gmail.com>
2017-02-12 15:07:29 -0800

    check_if_substring
    check if a string is substring of another string

--- checkifSubString.js
+++ checkifSubString.js
@@ -1,6 +1,21 @@
+//using String.prototype.seacrh
+//it returns -1 if not found; >1 if the value is there
 function isSubString(str1,str2){
-
-
-
-    
+if(str1.search(str2) != -1){
+    return true;
 }
+else return false;
+}
+
+//using String.prototype.include --> ES6
+//includes return true/false;
+function isSubString_Include(str1,str2){
+return str1.includes(str2);
+}
+
+//Test cases
+console.log(isSubString("Hello world","wor"));
+console.log(isSubString("Hello world","worf"));
+
+console.log(isSubString_Include("Hello world","wor"));
+console.log(isSubString_Include("Hello world","worf"));
